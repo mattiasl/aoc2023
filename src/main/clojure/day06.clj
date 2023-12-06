640c78fa0a7222709e299bff014b1ced96ac21ed
Mattias Liljeström <mattias.liljestrom@tietoevry.com>
2023-12-06 08:09:50 +0100

    day06 - remove unused code

--- src/main/clojure/day06.clj
+++ src/main/clojure/day06.clj
@@ -3,11 +3,6 @@
 
 (def lines (split (slurp "./src/main/clojure/inputs/day06.in") #"\n"))
 
-(defn parse [races]
-  (->> (map #(re-seq #"\d+" %) races)
-       (map #(map read-string %))
-       (apply map vector)))
-
 (defn count-distances [[t d]]
   (->> (reduce (fn [a c] (let [race-time (- t c)] (conj a (* c race-time)))) [0] (range 1 t))
        (filter #(< d %))
