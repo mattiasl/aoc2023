c7c5d25cce6df193ab997b28e39b0c4157891a3b
Mattias Liljeström <mattias.liljestrom@tietoevry.com>
2023-12-07 21:15:16 +0100

    day06 - add todo

--- src/main/clojure/day06.clj
+++ src/main/clojure/day06.clj
@@ -3,17 +3,18 @@
 
 (def lines (split (slurp "./src/main/clojure/inputs/day06.in") #"\n"))
 
+;TODO use maths to solve this with p-q-formula
 (defn count-distances [[t d]]
   (->> (reduce (fn [a c] (let [race-time (- t c)] (conj a (* c race-time)))) [0] (range 1 t))
        (filter #(< d %))
        (count)))
 
 ;part 1
-(->> (map #(re-seq #"\d+" %) lines)
+(time (->> (map #(re-seq #"\d+" %) lines)
      (map #(map read-string %))
      (apply map vector)
      (map count-distances)
-     (reduce *))
+     (reduce *)))
 
 ;part 2
 (time (->> (map #(re-seq #"\d+" %) lines)
